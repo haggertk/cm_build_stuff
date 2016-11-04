ad8ee594a91e6cd4f75d928532dadcd1e35f806c
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-11-04 05:26:03 -0600

    Re-enable sdcardfs
    Change-Id: Id79acc0c680e8f90d3a6c42318218276717369de

--- picks.sh
+++ picks.sh
@@ -12,6 +12,9 @@ repopick 167484
 # NOTE: Pretty close to done enough for me, I like the text-only option
 repopick 167464
 
+# sepolicy: allow sdcard access via sdcardfs
+repopick 168504
+
 # klte-common: Enable legacy mediaserver
 # NOTE: Depends on 167485, 167484
 repopick 167511
@@ -19,3 +22,9 @@ repopick 167511
 # [DO NOT MERGE] Revert "klte: Enable blur"
 # NOTE: Temp until 167370 is merged, then drop
 repopick 167512
+
+# Revert "Revert "klte-common: enable sdcardfs""
+repopick 168974
+
+# klte-common: sepolicy: Add rules for sdcardfs external SD card
+repopick 169069
