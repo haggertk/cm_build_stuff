1f9428270ddc8c4eb50fc246c5ff4849d283ba35
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-11-04 05:08:48 -0600

    Remove 167497 from picklist
    * It was abandoned from msm8974-common and merged to klte-common
    
    Change-Id: Ifed779798463ad916289d6ea6a7acb26920549bc

--- picks.sh
+++ picks.sh
@@ -12,10 +12,6 @@ repopick 167484
 # NOTE: Pretty close to done enough for me, I like the text-only option
 repopick 167464
 
-# msm8974-common: Add vsync phase offset
-# NOTE: Not absolutely required
-repopick 167497
-
 # klte-common: Enable legacy mediaserver
 # NOTE: Depends on 167485, 167484
 repopick 167511
