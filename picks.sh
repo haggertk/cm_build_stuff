9946af3123a02ffa5d8dfea53d7eff2dfd09e362
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-11-07 07:13:59 -0700

    picks: Clean things out, we're nightly-ready now!
    * Only dragging framework commit that allows changing clock style
    
    Change-Id: Ie6e0e7867a3ffee89960e2784d6e5b5c39c065e7

--- picks.sh
+++ picks.sh
@@ -2,26 +2,6 @@
 
 source build/envsetup.sh
 
-# Add rules required for TARGET_HAS_LEGACY_CAMERA_HAL1
-repopick 167484
-
 # SysUI: Use VectorDrawable's for rendering battery
 # NOTE: Pretty close to done enough for me, I like the text-only option
 repopick 167464
-
-# klte-common: Enable legacy mediaserver
-# NOTE: Depends on 167484
-repopick 167511
-
-# [DO NOT MERGE] Revert "klte: Enable blur"
-# NOTE: Temp until 167370 is merged, then drop
-repopick 167512
-
-# klte-common: Add buildable libpn547_fw
-repopick 169338
-
-# klte: nfc: Update configs and build
-repopick 169331
-
-# klte: Use source-built libpn547 NFC firmware blob
-repopick 169353
