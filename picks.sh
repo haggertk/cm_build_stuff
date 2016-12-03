bd20da3fd79181946c769ba5171e180ae5d250bc
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-12-03 10:21:20 -0700

    picks: Various updates
    * 167464 removed because merged
    * A bunch of others added for testing
    
    Change-Id: I0c40d85b24f27ac0e5d767bef620945709812ec6

--- picks.sh
+++ picks.sh
@@ -2,6 +2,20 @@
 
 source build/envsetup.sh
 
-# SysUI: Use VectorDrawable's for rendering battery
-# NOTE: Pretty close to done enough for me, I like the text-only option
-repopick 167464
+# liblights: Allow devices to override node paths
+repopick 172781
+
+# PhoneWindowManager: Implement press home to answer call
+repopick 174173
+
+# msm: clock-krait-8974: Add support to read SVS PVS bin
+repopick 174429
+
+# PM / devfreq: Check for GPU busy time before sending the sample
+repopick 174430
+
+# liblights: Remove device level HAL
+repopick 174076
+
+# klte-common: activate large_tcp_window_size
+repopick 174427
