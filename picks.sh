7dfb29e80d001b5ece8d8bf6391e783e42562335
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-12-04 16:01:13 -0700

    picks: Remove merged picks
    * 174173, 172781
    
    Change-Id: I4a29ebd0299818d501b5fa5137123d3145d11c3c

--- picks.sh
+++ picks.sh
@@ -2,20 +2,11 @@
 
 source build/envsetup.sh
 
-# liblights: Allow devices to override node paths
-repopick 172781
-
-# PhoneWindowManager: Implement press home to answer call
-repopick 174173
-
 # msm: clock-krait-8974: Add support to read SVS PVS bin
 repopick 174429
 
 # PM / devfreq: Check for GPU busy time before sending the sample
 repopick 174430
-
-# liblights: Remove device level HAL
-repopick 174076
 
 # klte-common: activate large_tcp_window_size
 repopick 174427
