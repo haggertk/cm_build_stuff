b69570f7cc4029be96225998db83e6046dbbf2f4
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2016-12-26 08:32:28 -0700

    picks: Remove all cherry-picks
    Change-Id: I90a5ed33a28ef15d238157a7fbabfbba821b5dd2

--- picks.sh
+++ picks.sh
@@ -2,11 +2,3 @@
 
 source build/envsetup.sh
 
-# msm: clock-krait-8974: Add support to read SVS PVS bin
-repopick 174429
-
-# PM / devfreq: Check for GPU busy time before sending the sample
-repopick 174430
-
-# klte-common: activate large_tcp_window_size
-repopick 174427
