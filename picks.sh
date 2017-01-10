729e2fb57ba954d44438ecac39ac7f85018431b2
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2017-01-10 06:28:23 -0700

    picks: Add some picks
    * Revert ion in kernel to fix camera
    * Try to fix the duos MSIM calling
    * Several trying to fix spr SMS/MMS. Yay Sprint.
    * selinux context fixes related to telephony symlinks
    
    Change-Id: If17d2e5140efed2826cfe7510001e4653108ac50

--- picks.sh
+++ picks.sh
@@ -3,3 +3,19 @@
 source build/envsetup.sh
 
 repopick -t rebrand
+
+# telephony: Add state check for LteOnCdma to isGsm and isCdma
+repopick 64932
+
+# Telecomm: Set MSIM audio params using the proper sub IDs
+# NOTE: I have this locally already with some additional debugging statements
+#repopick 155020
+
+# kltespr: Add overlay for config_volte_replacement_rat
+repopick 151854
+
+# klte-common: Fix file contexts related to our telephony symlink mess
+repopick 155643
+
+# Revert "gpu: ion: remove IOMMU Heap and IOMMUCA introduced with mm source"
+repopick 155896
