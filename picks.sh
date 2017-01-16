3e942ea4bf717d67e1c54fd9339f4f82d266bb49
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2017-01-15 20:29:41 -0700

    picks: Update picks
    * Drop some of the CDMA ones, they killed SPR data
    * Add some that (maybe) fix rotation on klte
    
    Change-Id: Ie05b9165ee03de9181f5ef4bbaa94473aa5bdaeb

--- picks.sh
+++ picks.sh
@@ -5,17 +5,16 @@ source build/envsetup.sh
 repopick -t rebrand
 
 # telephony: Add state check for LteOnCdma to isGsm and isCdma
-repopick 64932
+#repopick 64932
 
 # Telecomm: Set MSIM audio params using the proper sub IDs
-# NOTE: I have this locally already with some additional debugging statements
-#repopick 155020
+repopick 155020
 
 # kltespr: Add overlay for config_volte_replacement_rat
-repopick 151854
+#repopick 151854
 
-# klte-common: Fix file contexts related to our telephony symlink mess
-repopick 155643
+# sensors: Allow opting out of default batch size
+repopick 156735
 
-# Revert "gpu: ion: remove IOMMU Heap and IOMMUCA introduced with mm source"
-repopick 155896
+# klte-common: Fix rotation sensor
+repopick 156738
