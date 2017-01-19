0b05d068418caea4c13b7fe6ec81fce98e03bfee
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2017-01-18 18:05:35 -0700

    picks: Update picks
    * Remove bootanimation and MSIM audio picks since they are merged
    
    Change-Id: I6e19dea37480f295cf37dc2cf01275f047755416

--- picks.sh
+++ picks.sh
@@ -7,11 +7,6 @@ repopick -t rebrand
 # telephony: Add state check for LteOnCdma to isGsm and isCdma
 #repopick 64932
 
-# Telecomm: Set MSIM audio params using the proper sub IDs
-repopick 155020
-
 # kltespr: Add overlay for config_volte_replacement_rat
 #repopick 151854
 
-# vendor: lineage boot animation
-repopick 156893
