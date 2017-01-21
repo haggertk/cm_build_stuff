2ebb6f36e0d50344150fee7a82869c3fbe8cb2d1
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2017-01-21 12:12:42 -0700

    picks: Remove rebrand topic pick
    * Everything rebrand is committed, we're clean now
    
    Change-Id: I0e3cd6e60691e0f46b399f8ce32a73595af9243c

--- picks.sh
+++ picks.sh
@@ -2,11 +2,8 @@
 
 source build/envsetup.sh
 
-repopick -t rebrand
-
 # telephony: Add state check for LteOnCdma to isGsm and isCdma
 #repopick 64932
 
 # kltespr: Add overlay for config_volte_replacement_rat
 #repopick 151854
-
