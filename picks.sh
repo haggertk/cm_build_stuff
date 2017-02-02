0129e12c96b4b277e72f90ac7eba95719509d1e2
Kevin F. Haggerty <kevin.f.haggerty@gmail.com>
2017-02-02 02:10:32 -0700

    picks: Update picks
    * Add changes to squelch the "NOT SEANDROID ENFORCING" boot message
    * Add TWRP support change
    
    Change-Id: If76fafb2dba7548ea84e77b4d2873626d647a3c6

--- picks.sh
+++ picks.sh
@@ -7,3 +7,12 @@ source build/envsetup.sh
 
 # kltespr: Add overlay for config_volte_replacement_rat
 #repopick 151854
+
+# add a common mkbootimg.mk to quiet "KERNEL IS NOT SEANDROID ENFORCING" warning
+repopick 159752
+
+# klte-common: Quiet "KERNEL IS NOT SEANDROID ENFORCING" boot message
+repopick 159458
+
+# klte-common: Squashed set of TWRP support patches
+repopick 159705
