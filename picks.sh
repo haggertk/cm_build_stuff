31709ba8e17a6040f0c79bb9834810991453a0ab
Kevin F. Haggerty <haggertk@lineageos.org>
2017-09-10 18:23:58 -0600

    picks: Shift to Burn-In Protection only
    Change-Id: I450bde16925e77ba831087550292ea51a17221f5
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- picks.sh
+++ picks.sh
@@ -2,20 +2,5 @@
 
 source build/envsetup.sh
 
-# telephony: Add state check for LteOnCdma to isGsm and isCdma
-#repopick 64932
-
-# kltespr: Add overlay for config_volte_replacement_rat
-#repopick 151854
-
-# NfcNci: make T3T/Nfc-F HCE optional
-repopick 161917
-
-# klte-common: Disable T3T/Nfc-F HCE support
-repopick 161932
-
-# IMS: Phone switching
-repopick 163531
-
-# boringssl update
-repopick -t boringssl-update-from-master
+# Burn-In Protection
+repopick 187647 187648 187649
