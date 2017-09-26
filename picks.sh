b65699aef326c319ea12dcd9ecbf5bf77e3f21cb
Kevin F. Haggerty <haggertk@lineageos.org>
2017-09-25 19:32:03 -0600

    picks: Update pick list
    Change-Id: Id588b36aef7644226e38338a2db04660b778d0aa

--- picks.sh
+++ picks.sh
@@ -2,17 +2,15 @@
 
 source build/envsetup.sh
 
-for PICK in `curl https://gist.githubusercontent.com/invisiblek/85ebd8a96fba6086dbed3972557eba9e/raw/repopicks-lineage-15.0 | cut -d ' ' -f 1  | egrep "^[0-9]"`; do
-    repopick $PICK
-done
+#for PICK in `curl https://gist.githubusercontent.com/invisiblek/85ebd8a96fba6086dbed3972557eba9e/raw/repopicks-lineage-15.0 | cut -d ' ' -f 1  | egrep "^[0-9]"`; do
+#    repopick $PICK
+#done
 
-# Add roomservice
-repopick 185913
+# adb: Restore support for legacy f_adb interface
+repopick 185642
 
-# dtbhtool: Use libfdt from external/dtc
-repopick 186324
+# releasetools: don't assert device
+repopick 186687
 
-repopick -t releasetools-scripts
-
-# releasetools: ota_from_target_files: add FullOTA_PostValidate
-repopick 187374
+# Allow setting the recovery density separately from the aapt config
+repopick 190430
