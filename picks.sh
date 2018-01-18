bf93d421370223989e342395f5c2b780ce3184ee
Kevin F. Haggerty <haggertk@lineageos.org>
2018-01-18 07:29:34 -0700

    picks: Update, again
    * Drop merged/abandoned changes
    * Re-sort selinux changes
    * Add privacy guard and SU changes
    
    Change-Id: Ib5708937840c296392332780781bbed31765b58e

--- picks.sh
+++ picks.sh
@@ -30,9 +30,6 @@ kpick 201182 # klte-common: libril: Get off my back
 kpick 202457 # klte-common: HAXX: Fix seeming RIL start race condition
 kpick 199946 # [DNM] klte-common: sepolicy: Rewrite for O
 
-# hardware/samsung
-kpick 200068 # AdvancedDisplay: cyanogenmod -> lineageos
-
 # device/lineage/sepolicy
 kpick 198594 # sepolicy: qcom: Import bluetooth_loader/hci_attach rules
 kpick 199347 # sepolicy: Set the context for fsck.exfat/ntfs to fsck_exec
@@ -48,12 +45,12 @@ kpick 199517 # sepolicy: Permissions for userinit
 kpick 199518 # sepolicy: Fix sysinit denials
 kpick 199571 # sepolicy: Move fingerprint 2.0 service out of private sepolicy
 kpick 199572 # sepolicy: SELinux policy for persistent properties API
+kpick 201552 # Squashed import of superuser SELinux policies
+kpick 201582 # sepolicy: adapt sudaemon policy for O
 
 # device/qcom/sepolicy
-kpick 202377 # Revert "sepolicy: Address netmgrd denials on non-fully trebelized devices"
-kpick 202378 # legacy: add back perfd sepolicy
+kpick 199557 # legacy: add back perfd sepolicy
 kpick 202379 # legacy: add back radio rules
-kpick 202380 # legacy: add back nfc rules
 kpick 202381 # legacy: add back rules for non-treble devices
 kpick 202382 # legacy: allow rmt_storage sys_admin capability
 kpick 202383 # legacy: let rfs_access do msm ipc ioctls
@@ -66,22 +63,29 @@ kpick 202389 # legacy: Fix labeling the thermal sockets
 kpick 202390 # legacy: let audioserver connect to thermal engine sockets
 kpick 202391 # legacy: label per_mgr as a binder service
 kpick 198303 # sepolicy: Add sysfs labels for devices using 'soc.0'
-kpick 199564 # sepolicy: Allow energyawareness to read sysfs files
-kpick 199554 # sepolicy: Add /data/vendor/time label for old oreo blobs
-kpick 199559 # sepolicy: Allow dataservice_app to read/write to IPA device
+kpick 202824 # legacy: Readd support for old perfd socket
+kpick 202825 # legacy: Add back old fdAlbum rule
+kpick 202826 # sepolicy: Label boot/recovery/cache/system partitions
+kpick 202827 # legacy: Label old UIO sysfs
+kpick 202828 # legacy: Allow qmuxd access diag
+kpick 202829 # legacy: Label old SSR sysfs
+kpick 202830 # legacy: Add back legacy sensors rules
+kpick 202831 # legacy: Label old kgsl sysfs nodes
+kpick 202995 # legacy: Address mpdecision denials
 kpick 198141 # Use set_prop() macro for property sets
-kpick 202104 # DNM: Squashed update to LA.UM.6.4.r1-05700-8x98.0
 
 # system/sepolicy
 kpick 199664 # sepolicy: Fix up exfat and ntfs support
 kpick 198106 # Add rules required for TARGET_HAS_LEGACY_CAMERA_HAL1
 kpick 198107 # Adapt add_service uses for TARGET_HAS_LEGACY_CAMERA_HAL1
 kpick 198108 # mediaserver: Allow finding the hal_camera hardware service
+kpick 201553 # sepolicy: We need to declare before referencing
+kpick 201583 # sepolicy: Allow su by apps on userdebug_or_eng
+kpick 201584 # sepolicy: update policies for sudaemon on O
+kpick 201732 # sepilocy: add sudaemon to ignore list
 
 # system/core
-kpick 202493 # init: add detection of charging mode
-kpick 202494 # init: define BOARD_CHARGING_CMDLINE parameters
-kpick 202495 # init: Bring back support for arbitrary chargermode cmdlines
+kpick 202849 # Update permissions to the superuser binary
 d=`pwd`
 cd system/core || exit 1
 git remote remove blek > /dev/null 2>&1
@@ -91,25 +95,32 @@ git cherry-pick 96c4433e || exit 1 # init: I hate safety net
 cd "$d" || exit 1
 
 # frameworks/base
-kpick 199947 # PowerManager: Re-integrate button brightness
 kpick 200968 # statusbar: Add arguments to shutdown and reboot to allow confirmation
 kpick 200969 # SystemUI: Power menu customizations
+kpick 201879 # frameworks: Privacy Guard for O
+kpick 199947 # PowerManager: Re-integrate button brightness
 
 # frameworks/av
 kpick 198113 # camera/media: Support for legacy camera HALv1
 kpick 198114 # libstagefright: Support for legacy camera/encoder buffers
 kpick 198116 # CameraService: Fix deadlock in binder death cleanup.
 
+# frameworks/native
+kpick 201530 # AppOpsManager: Update with the new ops
+
 # packages/apps/Settings
 kpick 199839 # Settings: Add advanced restart switch
+kpick 201529 # Settings: Privacy Guard
+kpick 201531 # Settings: Add developer setting for root access
+
 
 # packages/apps/LineageParts
-kpick 199948 # LineageParts: Bring up button backlight settings
 kpick 201309 # LineageParts: Re-enable PowerMenuActions and adapt to SDK updates
+kpick 201528 # PrivacyGuard: Bring up and inject into Settings
+kpick 199948 # LineageParts: Bring up button backlight settings
 
 # lineage-sdk
-kpick 200970 # sdk: Move isAdvancedRebootEnabled to SDK from global access
-kpick 201311 # lineage-sdk: Add broadcast action for power menu update
+kpick 200970 # lineage-sdk: Import power menu related classes
 
 # vendor/lineage
 kpick 201336 # soong_config: Add TARGET_HAS_LEGACY_CAMERA_HAL1 variable
