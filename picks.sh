1be1478fab1a603cc100737dce5f9158e33f6e28
Kevin F. Haggerty <haggertk@lineageos.org>
2018-02-12 20:42:57 -0700

    picks: Update picks for unstaging
    * Sort by device vs. platform as well as alphabetically
    
    Change-Id: I44cc0358bd06c3bff185798eb37c596f9b31f856

--- picks.sh
+++ picks.sh
@@ -6,15 +6,36 @@ function kpick() {
 
 source build/envsetup.sh
 
+# -------------- DEVICE STUFF --------------
+
 # device/samsung/hlte-common
 kpick 205333 # hlte-common: Add hltekor to makefile guards
 
 # device/samsung/msm8974-common
 kpick 205468 # msm8974-common: libril: Fix RIL_Call struct for 64-bit platforms
 
-# system/sepolicy
-kpick 198106 # Add rules required for TARGET_HAS_LEGACY_CAMERA_HAL1
-kpick 198107 # Adapt add_service uses for TARGET_HAS_LEGACY_CAMERA_HAL1
+# kernel/samsung/msm8974
+kpick 205073 # fs: proc: Remove Samsung changes to commandline
+
+# -------------- PLATFORM STUFF --------------
+
+# frameworks/av
+kpick 206430 # CameraService: Fix deadlock in binder death cleanup.
+kpick 206431 # libstagefright: Free buffers on observer died
+kpick 206432 # Camera: fix use after disconnect error
+kpick 206433 # stagefright: ACodec: Resolve empty vendor parameters usage
+kpick 206434 # media: fix infinite wait at source for HAL1 based recording
+kpick 206435 # libstagefright: use 64-bit usage for native_window_set_usage
+kpick 206427 # camera/media: Support legacy HALv1 camera in mediaserver
+
+# frameworks/base
+kpick 206131 # NightDisplayTile: disable if LiveDisplay feature is available
+
+# packages/apps/Nfc
+kpick 206115 # NfcNci: make T3T/Nfc-F HCE optional
+
+# packages/apps/Settings
+kpick 206403 # settings: Kill night mode if we have LiveDisplay
 
 # system/core
 d=`pwd`
@@ -25,27 +46,11 @@ git fetch blek lineage-15.1_safetynet || exit 1
 git cherry-pick 96c4433e || exit 1 # init: I hate safety net
 cd "$d" || exit 1
 
-# frameworks/av
-kpick 198116 # CameraService: Fix deadlock in binder death cleanup.
-kpick 203520 # media: fix infinite wait at source for HAL1 based recording
-kpick 203521 # libstagefright: Free buffers on observer died
-kpick 203522 # stagefright: ACodec: Resolve empty vendor parameters usage
-kpick 201731 # libstagefright: use 64-bit usage for native_window_set_usage
-kpick 203523 # Camera: fix use after disconnect error
-kpick 198113 # camera/media: Support legacy HALv1 camera in mediaserver
-
-# frameworks/opt/telephony
-kpick 200797 # PhoneFactory: fix creating a cdma phone type
-
-# kernel/samsung/msm8974
-kpick 205073 # fs: proc: Remove Samsung changes to commandline
-
-# packages/apps/Settings
-kpick 199922 # settings: Kill night mode if we have LiveDisplay
+# system/sepolicy
+kpick 206428 # Add rules required for TARGET_HAS_LEGACY_CAMERA_HAL1
+kpick 206429 # Adapt add_service uses for TARGET_HAS_LEGACY_CAMERA_HAL1
 
 # vendor/lineage
-kpick 201336 # soong_config: Add TARGET_HAS_LEGACY_CAMERA_HAL1 variable
-
-repopick -t berry
+kpick 206426 # soong_config: Add TARGET_HAS_LEGACY_CAMERA_HAL1 variable
 
 exit 0
