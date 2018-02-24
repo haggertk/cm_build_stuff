48d05912b1cdce9cf06531e170ef0023c3c5ac1e
Kevin F. Haggerty <haggertk@lineageos.org>
2018-02-24 07:58:57 -0700

    picks: Clean out merged changes
    Change-Id: Ie605cedbdcf93e0c355679f6b6aa7ac4218c030c

--- picks.sh
+++ picks.sh
@@ -9,27 +9,18 @@ source build/envsetup.sh
 # -------------- DEVICE STUFF --------------
 
 # device/samsung/hlte-common
-kpick 207220 # hlte-common: Update kernel exfat flag
+kpick 207147 # hlte-common: Use sdfat for exfat
 
-# device/samsung/hlte-common
-kpick 207271 # klte-common: Update kernel exfat flag
+# device/samsung/klte-common
 
 # device/samsung/msm8974-common
-kpick 207146 # msm8974-common: Remove exfat and ntfs sepolicies
 
 # kernel/samsung/msm8974
-kpick 207142 # fs: Add sdfat
-kpick 207143 # fs: sdfat: Fix compilation on Linux 3.4
-kpick 207144 # fs: exfat: Allow disabling exfat
-kpick 207145 # ARM: configs: lineage_*: Enable sdfat
-kpick 207162 # fs: sdfat: Fix compilation without debugging
-kpick 207163 # fs: sdfat: Allow disabling sdfat
+kpick 207529 # misc: Import SM-G900F kernel source code
+kpick 207530 # msm: camera_ll: Pick G900FXXU1CRA2 camera_v2 changes
+kpick 207532 # net: wireless: bcmdhd: Drop back to 2930b42b636 state
 
 # -------------- PLATFORM STUFF --------------
-
-# devide/lineage/sepolicy
-kpick 207148 # sepolicy: Cleanup context labelling for external sdcards
-kpick 207128 # common: Label sdfat as vfat
 
 # frameworks/av
 kpick 206430 # CameraService: Fix deadlock in binder death cleanup.
@@ -39,15 +30,6 @@ kpick 206433 # stagefright: ACodec: Resolve empty vendor parameters usage
 kpick 206434 # media: fix infinite wait at source for HAL1 based recording
 kpick 206435 # libstagefright: use 64-bit usage for native_window_set_usage
 kpick 206427 # camera/media: Support legacy HALv1 camera in mediaserver
-
-# frameworks/base
-kpick 206131 # NightDisplayTile: disable if LiveDisplay feature is available
-
-# packages/apps/Nfc
-kpick 206115 # NfcNci: make T3T/Nfc-F HCE optional
-
-# packages/apps/Settings
-kpick 206403 # settings: Kill night mode if we have LiveDisplay
 
 # system/core
 d=`pwd`
@@ -61,13 +43,6 @@ cd "$d" || exit 1
 # system/sepolicy
 kpick 206428 # Add rules required for TARGET_HAS_LEGACY_CAMERA_HAL1
 kpick 206429 # Adapt add_service uses for TARGET_HAS_LEGACY_CAMERA_HAL1
-kpick 207219 # Revert "sepolicy: Add missing exfat and fuseblk types"
-kpick 207150 # Revert "sepolicy: Fix up exfat and ntfs support"
-
-# system/vold
-kpick 207151 # vold: Make exfat driver support generic
-kpick 207154 # vold: Don't disable dirsync for exfat
-kpick 207240 # vold: Only include exfat support for devices that specify a driver
 
 # vendor/lineage
 kpick 206426 # soong_config: Add TARGET_HAS_LEGACY_CAMERA_HAL1 variable
@@ -75,8 +50,6 @@ kpick 206996 # soong_config: Add TARGET_USES_MEDIA_EXTENSIONS variable
 
 # -------------- TOPIC STUFF --------------
 
-repopick -t advanced-display || exit 1
-repopick -t advanced-display-samsung-8974 || exit 1
 repopick -t oreo-mr1-wallpapers || exit 1
 
 exit 0
