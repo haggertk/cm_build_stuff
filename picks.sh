2bc7e9bec533c84e3c1284088d59e95bcc605ffe
Kevin F. Haggerty <haggertk@lineageos.org>
2018-03-20 06:40:50 -0600

    picks: Add mata-related changes
    Change-Id: I7552ebd64d74e712b41a67f2b14610fdca35b881

--- picks.sh
+++ picks.sh
@@ -13,6 +13,7 @@ kpick 208979 # bullhead: Update makefile guards to allow child variants
 kpick 208980 # bullhead: Make setup-makefiles imitate a common repository version
 
 # device/samsung/hlte-common
+kpick 209612 # hlte-common: Set BOARD_CUSTOM_BOOTIMG to true
 kpick 207147 # hlte-common: Use sdfat for exfat
 
 # device/samsung/klte-common
@@ -21,9 +22,20 @@ kpick 207883 # klte-common: Use sdfat for exfat
 # device/samsung/msm8974-common
 
 # kernel/samsung/msm8974
+kpick 209262 # fs: sdfat: Update to version 2.0.6-lineage
+kpick 209263 # fs: sdfat: Fix compilation for 32-bit targets
 kpick 207265 # ARM: configs: lineage_*: Disable exfat and vfat
 
+# mata stuff
+kpick 209367 # mata: fix hbtp_grip script permission
+kpick 204570 # mata: Build textclassifier's smart selection
+kpick 204877 # mata: add ratchet_rat_families carrier config setting
+kpick 206118 # InputMethodManagerService: adjust grip mode for input enable/disable
+#kpick 206121 # [HAX] remove stylus shit from touchscreen input
+
 # -------------- PLATFORM STUFF --------------
+
+# build
 
 # external/toybox
 kpick 209019 # toybox: Use ISO C/clang compatible __typeof__ in minof/maxof macros
@@ -44,9 +56,17 @@ kpick 206119 # init: I hate safety net
 kpick 206428 # Add rules required for TARGET_HAS_LEGACY_CAMERA_HAL1
 kpick 206429 # Adapt add_service uses for TARGET_HAS_LEGACY_CAMERA_HAL1
 
+# updater
+kpick 206046 # update_attempter: don't limit the cpu and run in foreground
+kpick 206117 # update_verifier: skip verity to determine successful on lineage builds
+kpick 206128 # common: add update_engine policies
+kpick 206681 # Updater: fix bad state when opening activity during A/B update
+kpick 206682 # Updater: unbind stale callbacks when reconnecting to A/B
+
 # vendor/lineage
 kpick 206426 # soong_config: Add TARGET_HAS_LEGACY_CAMERA_HAL1 variable
 kpick 206996 # soong_config: Add TARGET_USES_MEDIA_EXTENSIONS variable
+kpick 206638 # extract_utils: Use vdexExtractor and oatdump for deodexing
 
 # -------------- TOPIC STUFF --------------
 repopick -Q "branch:lineage-15.1+topic:sdclang+status:open"
