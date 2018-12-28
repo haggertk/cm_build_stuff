d0b3df2a1c58ed9e1288d2eb3a63636e2bd73723
Kevin F. Haggerty <haggertk@lineageos.org>
2018-12-28 15:32:18 -0700

    picks: Major updates
    * Remove (seemingly) unncessary changes
    * Remove merged/abandoned changes
    
    Change-Id: Iabc06f6ac5f97f019402da6c595c6952dd9d354b
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>

--- picks.sh
+++ picks.sh
@@ -28,16 +28,13 @@ fi
 
 # device/samsung/klte-common
 if [ -d device/samsung/klte-common ] ; then
-  repopick 231209 # klte-common: nfc: pn547: Use prebuilt NFC HAL from 15.1
   repopick 225192 # klte-common: Align ril.h to samsung_msm8974-common P libril changes
-  repopick 224917 # DO NOT MERGE: klte-common: Requisite bring-up BS change
 fi
 
 # device/samsung/msm8974-common
 if [ -d device/samsung/msm8974-common ] ; then
-  repopick 235140 # msm8974-common: sepolicy: Clean up
   repopick 235457 # msm8974-common: sepolicy: Limit execmod to specifically labeled files
-  repopick 234526 # msm8974-common: sepolicy: Resolve mediaserver denials
+  repopick 237215 # msm8974-common: sepolicy: Eliminate qemu_hw_mainkeys_prop entries
 fi
 
 # kernel/samsung/msm8974
@@ -49,72 +46,43 @@ fi # [ $USER != haggertk ]
 
 # -------------- PLATFORM STUFF --------------
 
-# build/make
-repopick 222760 # Add LOCAL_AIDL_FLAGS
-repopick 222742 # build: Use project pathmap for recovery
-
-# build/soong
-repopick 224613 # soong: Add LOCAL_AIDL_FLAGS handling
-
 # device/lineage/sepolicy
-repopick 235402 # common: Allow init to relabel I/O sched tuning nodes
-repopick 234613 # common: common: Expand labeling of sysfs_vibrator nodes using genfscon
+repopick 234613 # common: Expand labeling of sysfs_vibrator nodes using genfscon
 repopick 234837 # common: Label and allow access over LiveDisplay sysfs nodes
-
-# device/qcom/sepolicy
-repopick 228570 # sepolicy: Allow wcnss_service to set wlan.driver properties
-repopick 228572 # sepolicy: Allow system_server to 'read' qti_debugfs
-repopick 228573 # sepolicy: Add libsdm-disp-vndapis and libsdmutils to SP-HALs
-repopick 228576 # sepolicy: Label mpctl_socket as data_file_type
-repopick 228578 # sepolicy: rules to allow camera daemon access to app buffer
-repopick 228580 # hal_gnss_default: Do not log udp socket failures
-repopick 228582 # sepolicy: qti_init_shell needs to read dir too
-repopick 228583 # sepolicy: allow vold to read persist dirs
+repopick 236446 # common: Improve label of I/O sched tuning nodes
 
 # device/qcom/sepolicy-legacy
 repopick 230235 # common: grant DRM HIDL HAL ownership access to /data/{misc,vendor}/media/
-repopick 235455 # legacy: Allow platform_app to read qemu_hw_mainkeys_prop
+repopick 237211 # Remove vendor se-labelling of AOSP's qemu.hw.mainkeys prop.
 
 # frameworks/base
-repopick 224513 # SystemUI: Disable config_keyguardUserSwitcher on sw600dp
 repopick 224266 # SystemUI: Add Lineage statusbar item holder
 repopick 224267 # SystemUI: Network Traffic [1/3]
-repopick 226343 # CameraServiceProxy: Loosen UID check
-
-# frameworks/native
-repopick 224443 # libbinder: Don't log call trace when waiting for vendor service on non-eng builds
-repopick 225546 # AppOpsManager: Update with the new ops
-
-# hardware/interfaces
-repopick 225506 # Camed HAL extension: Added support in HIDL for Extended FD.
-repopick 225507 # camera: Only link and use vendor.qti.hardware.camera.device if specified
-
-# hardware/lineage/interfaces
-repopick 223374 # interfaces: Add 2.0 livedisplay interfaces
-repopick 223410 # interfaces: Add touch HIDL interface definitions
-
-# packages/apps/Camera2
-repopick 224752 # Use mCameraAgentNg for getting camera info when available
-repopick 225265 # Add Storage preference (1/2)
+repopick 235986 # frameworks: Add unlinked ringtone and notification volumes
+repopick 233633 # Phone ringtone setting for Multi SIM device
+#repopick 226343 # CameraServiceProxy: Loosen UID check
 
 # packages/apps/Settings
 repopick 226148 # Settings: "Security & location" -> "Security & privacy"
-repopick 226142 # Settings: Add developer setting for root access
-repopick 226151 # Settings: show Trust brading in confirm_lock_password UI
+repopick 236550 # fingerprint: Remove unnecessary spacing in enroll layout
 repopick 226154 # fingerprint: Allow devices to configure sensor location
 repopick 227120 # Settings: Check interfaces before enabling ADB over network
-repopick 231590 # SimSettings: Add manual SIM provisioning support
+repopick 233634 # Phone ringtone setting for Multi SIM device
+repopick 226142 # Settings: Add developer setting for root access
+repopick 235978 # Settings: Add switch for linked ring and media notification volumes
+repopick 236184 # Settings: Use correct icon for ring volume
+
+# packages/services/Telecomm
+repopick 233635 # Phone ringtone setting for Multi SIM device
 
 # system/core
 privpick system/core refs/changes/19/206119/2 # init: I hate safety net
 
 # system/sepolicy
+repopick 237226 # Allow mediaserver to read device directories
 repopick 234884 # Allow init to write to /proc/cpu/alignment
 repopick 234886 # Allow init to chmod/chown /proc/slabinfo
 repopick 235196 # Allow dnsmasq to getattr netd unix_stream_socket
-
-# vendor/lineage
-repopick 225921 # overlay: Update list of GSF/GMS activities
 
 # -------------- TOPIC STUFF --------------
 
