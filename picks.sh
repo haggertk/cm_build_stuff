48978782f6f8c177953e809e07d86bd307302848
Kevin F. Haggerty <haggertk@lineageos.org>
2020-11-30 10:04:37 -0700

    picks: Update picks
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>
    Change-Id: I9be7d8a05298c2260904231d83ef1f42f8ccd1e4

--- picks.sh
+++ picks.sh
@@ -50,27 +50,25 @@ repopick 288657 # Sorry bro: 6 -> 3
 # build/soong
 repopick 290065 # Add ability to enable scudo-free 32-bit libc variant.
 
-# frameworks/av
-repopick 288412 # nuplayer: Avoid crash when codec fails to load
-repopick 288413 # camera: Don't segfault if we get a NULL parameter
-repopick 288414 # libstagefright: Support YVU420SemiPlanar camera format
-repopick 288415 # stagefright: omx: Don't signal dataspace change on legacy QCOM
-repopick 288416 # stagefright: ACodec: Resolve empty vendor parameters usage
-repopick 288417 # libstagefright: Free buffers on observer died
-repopick 288418 # libstagefright: use 64-bit usage for native_window_set_usage
-repopick 288419 # camera: include: Don't override possible overlayed header
-repopick 288420 # camera/media: Support legacy HALv1 camera in mediaserver
-repopick 288421 # Camera: check metadata type before releasing frame
-repopick 289200 # libstagefright: Fix memory leak due to lock timeout
-repopick 289171 # camera: Allow devices to load custom CameraParameter code
-
 # frameworks/native
 repopick 291971 # SurfaceFlinger: Don't cleanup resource from previous frame
 
-# hardware/lineage/interfaces
-repopick 287659 # interfaces: Camera: add IAllocator/IMapper 4.0 to camera HIDL
-repopick 287660 # interfaces: camera: fix two potential null pointer uses
-repopick 287661 # interfaces: camera: Implement lockYCbCr stub for Mapper 4.0
+# hardware/qcom-caf/msm8974/audio
+repopick 293116 # audio: Add audio headers lib
+repopick 293117 # audio: Include media headers
+
+# hardware/qcom-caf/msm8974/display
+repopick 293118 # display: Add display headers lib
+
+# hardware/qcom-caf/msm8974/media
+repopick 293114 # media: Add media headers lib
+repopick 293115 # media: Include display headers
+
+# packages/apps/Gallery2
+repopick -p 294650 # Gallery2: Remove LOCAL_NDK_STL_VARIANT
+
+# packages/apps/Snap
+repopick -p 294911 # Snap: Remove saving to sdcard
 
 # system/core
 repopick 292788 # core: Bring back support for legacy FunctionFS
@@ -90,8 +88,7 @@ repopick 286954 # vold: skip first disk change when converting MBR to GPT
 repopick 286955 # vold: Accept Linux GPT partitions on external SD cards
 
 # vendor/lineage
-repopick 291988 # qcom: Enable TARGET_USES_QCOM_BSP_LEGACY for B family
-repopick 292258 # soong: Make QCOM legacy variable name more generic
+repopick 291988 # soong: Add flag for disabling postrender cleanup
 
 # -------------- TOPIC STUFF --------------
 
