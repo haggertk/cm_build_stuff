d8fbfab15270828113715d885b641b7ee0027c78
Kevin F. Haggerty <haggertk@lineageos.org>
2022-03-20 07:30:06 -0600

    picks: Update for 19.1 branch
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>
    Change-Id: I41317cecd439d9cecd3625671f64843a96554d0e

--- picks.sh
+++ picks.sh
@@ -18,24 +18,17 @@ source build/envsetup.sh
 
 # -------------- PLATFORM STUFF --------------
 
+# external/chromium-webview
+repopick 325922 # Update arm{,64} Chromium Webview to 99.0.4844.73
+
 # frameworks/base
-repopick 317786 # monet: Add support for monet (cam16)
 repopick 318763 # SystemUI: runtime configurable audio panel location
 repopick 321160 # Keyguard: Allow disabling fingerprint wake-and-unlock
 repopick 320714 # SystemUI: add burnIn protection
 
 # packages/apps/Trebuchet
-repopick 317966 # Enable new app open/close animations
 repopick 317967 # Enable crossfade when changing theme
-repopick 317968 # Enable new keyguard-to-launcher animation
 repopick 317969 # Expose themed icon setting in ThemePicker
-repopick 318383 # Enable local colors for popup menu items
-repopick 318384 # Add ripple animation to task menu items
-repopick 320939 # Implement LocalColorExtractor using monet theme engine
-
-# vendor/lineage
-repopick 318283 # overlay: core: Remove accent color overrides
-repopick 317788 # overlay: Enable monet
 
 # -------------- TOPIC STUFF --------------
 
