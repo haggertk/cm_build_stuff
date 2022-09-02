e51fff65986956cf9bac5d23cfc1360726618dd6
Kevin F. Haggerty <haggertk@lineageos.org>
2022-09-02 10:31:05 -0600

    picks: Clean out for 20
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>
    Change-Id: I7112e3d3cff122e5487dbaaa6c234214ec22bfdb

--- picks.sh
+++ picks.sh
@@ -19,14 +19,8 @@ source build/envsetup.sh
 # -------------- PLATFORM STUFF --------------
 
 # frameworks/base
-#repopick 318763 # SystemUI: runtime configurable audio panel location
-repopick 320714 # SystemUI: add burnIn protection
 privpick frameworks/base baf2e2f48a4ef6b64e67ad6dfd7740113058aad7 # Alter model name to avoid SafetyNet HW attestation enforcement
 privpick frameworks/base f96f3f09b4cadfbf0474ab633ad33f55c6449e90 # keystore: Block key attestation for SafetyNet
-
-# packages/apps/Trebuchet
-repopick 317967 # Enable crossfade when changing theme
-repopick 317969 # Expose themed icon setting in ThemePicker
 
 # -------------- TOPIC STUFF --------------
 
